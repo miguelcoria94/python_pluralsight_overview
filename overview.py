261a65feb0962b65a014c96caa33f0abd232c6bb
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-22 02:23:15 -0600

    working with generators

--- overview.py
+++ overview.py
@@ -1,1 +1,14 @@
-print('Hello Python!')
+##print('Hello Python!')
+
+def my_decorator(func):
+    def wrap_func():
+        print('*****')
+        func()
+        print('*****')
+    return wrap_func
+
+@my_decorator
+def hello():
+    print('Hellooo')
+    
+hello()
